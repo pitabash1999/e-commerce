50666f55563fd1768564161a4b246dad5a86c9e0
Pitabash <pitabashk196@gmail.com>
2025-11-06 22:22:50 +0530

    completed the service discovery

---
+++ service-discovery/src/test/java/com/eCommerce/main/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eCommerce.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
